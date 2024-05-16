121277804edc03fc999bd136c36bd5d94542092b
oskaars <oskarskoora@gmail.com>
2024-05-16 18:16:34 +0200

    16.05 working on html layout ADDED github button

--- src/App.js
+++ src/App.js
@@ -1,53 +1,23 @@
-import React, { useState } from 'react';
-import './App.css';
 
-function MyButton({ onClick, text }) {
-  return (
-    <button type="button" onClick={onClick}>
-      {text}
-    </button>
-  );
-}
+import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+
+
+
+import HomePage from './components/Home';
+import NotFoundPage from './components/NotFoundPage';
 
 
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  function handleLogin() {
-    setIsLoggedIn(true);
-  }
-
-  function handleLogout() {
-    setIsLoggedIn(false);
-  }
-
-  let login;
-  let register
-  if (isLoggedIn) {
-    login = <MyButton onClick={handleLogout} text="Sign out" />;
-  } else {
-    login = <MyButton onClick={handleLogin} text="Sign in" /> ;
-
-  }
-
-  return (
-    <div className="parent">
-      <div className='title'>
-      <h1>HOOPERS </h1>
-      </div>
-      
-      <div className='login-box'>
-        {login} 
-        <button type="button" onClick="#">
-          Sign up
-        </button>
-      </div>
-
-      <div className='navBar'>
-        navbar
-      </div>
-    </div>
+  return(
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage  />} />
+        <Route path='*' element={<NotFoundPage  />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,13 @@
+import './404.css';
+
+
+function NotFoundPage(){
+    
+    return(
+    <div>
+        <h2>error: page not found</h2>
+    </div>
+    )
+}
+
+export default NotFoundPage;
